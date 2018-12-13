c8c34ffa3b0ce2a616f81984cdb0b2330a0f68d9
Муртазин Антон <44569595+ToxanUfa@users.noreply.github.com>
2018-12-14 01:16:47 +0300

    Add files via upload

---
+++ src/DepositCalculatorTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DepositCalculatorTest {
+
+	@Test
+	void testDeposit() {
+		DepositCalculator test = new DepositCalculator();
+		double result = test.Deposit(30000.0 , 10.0, 1);
+		assertEquals(33000, result);
+	}
+	
+	@Test
+	void testDeposit1() {
+		DepositCalculator test = new DepositCalculator();
+		double result = test.Deposit(30000.0 , 10.0, 1);
+		assertEquals(33000, result);
+	}
+	
+}
